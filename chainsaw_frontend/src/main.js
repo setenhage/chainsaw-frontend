4cc1d67bc13f6db23dbf99d41139b6e584b0a6dd
suus ten hage <suzanne.tenhage@gmail.com>
2023-09-23 19:23:00 +0200

    start login

--- chainsaw_frontend/src/main.js
+++ chainsaw_frontend/src/main.js
@@ -1,8 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth0 } from '@auth0/auth0-vue';
 
 const app = createApp(App)
 
 app.use(router)
-app.mount('#app')
+app.use(
+    createAuth0({
+      domain: "dev-c18h5mluplzm6pjc.us.auth0.com",
+      clientId: "jjBkLE8FuaMOQqdmsKPrKHpVwVuuUxCi",
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  );
+
+  app.mount('#app')
