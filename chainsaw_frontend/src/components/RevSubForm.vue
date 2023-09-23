e4f0b15af8abbfc075d61b954541cab0e5d8b104
prstolpe <pr.stolpe@gmail.com>
2023-09-23 17:33:53 +0200

    initial form commit

---
+++ chainsaw_frontend/src/components/RevSubForm.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+      <form @submit.prevent="submitForm" v-if="!formSubmitted">
+        <span>Street Address</span><br>
+        <input 
+          v-model="street_address"
+          type="text"
+          placeholder="Enter your streetname and house number" 
+        /><br>
+        <span>Postal Code</span><br>
+        <input 
+          v-model="postal_code"
+          type="text"
+          placeholder="Enter your postal code" 
+        /><br>
+        <span>Gender</span><br>
+        <input 
+          type="radio" 
+          v-model="gender" 
+          value="Male" 
+        />
+        <label>Male</label>
+        <input 
+          type="radio" 
+          v-model="gender" 
+          value="Female" 
+        />
+        <label>Female</label><br>
+        <input 
+          class="submit" 
+          type="submit" 
+          value="Submit"
+        >
+      </form>
+      <div v-if="formSubmitted">
+        <h3>Form Submitted</h3>
+        <p>Street Address: {{ street_address }}</p>
+        <p>Postal Cord: {{ postal_code }}</p>
+        <p>Gender: {{ gender }}</p>
+        <small>Click on run to launch the app again.</small>
+      </div>
+    </div>
+  </template>
+  <script>
+    export default {
+      data() {
+        return {
+          name: "",
+          email: "",
+          gender: "",
+          formSubmitted: false
+        };
+      },
+      methods: {
+        submitForm: function () {
+          this.formSubmitted = true
+        }
+      },
+    };
+  </script>
+  <style>
+    form {
+      padding: 10px;
+      border: 2px solid black;
+      border-radius: 5px;
+    }
+  
+    input {
+      padding: 4px 8px;
+      margin: 4px;
+    }
+  
+    span {
+      font-size: 18px;
+      margin: 4px;
+      font-weight: 500;
+    }
+  
+    .submit {
+      font-size: 15px;
+      color: #fff;
+      background: #222;
+      padding: 6px 12px;
+      border: none;
+      margin-top: 8px;
+      cursor: pointer;
+      border-radius: 5px;
+    }
+  
+  </style>
---
+++ chainsaw_frontend/src/views/revsubformview.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1> Submit your review </h1>
+    <div id="app">
+    <RevSubForm/>
+  </div>
+</template>
+
+
+<script>
+import RevSubForm from "../components/RevSubForm.vue";
+export default {
+  name: "revSubForm",
+  components: {
+    RevSubForm
+  }
+};
+</script>
+
+<style>
+
+</style>
